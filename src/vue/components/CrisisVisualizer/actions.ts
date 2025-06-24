0b282bd68bab77892ae8685808e02958b65e389b
Alvin Hamaïde <alvin98mc@gmail.com>
2025-06-24 23:29:10 +0200

    fix(action): deleted mock data

--- src/vue/components/CrisisVisualizer/actions.ts
+++ src/vue/components/CrisisVisualizer/actions.ts
@@ -3,5 +3,5 @@
 import { getInjection } from "@/di/container";
 
 export const save = async (duration: number, id: string) => {
-	return await getInjection("CrisisController").save(id, new Date(2025, 1, 1), duration);
+	return await getInjection("CrisisController").save(id, new Date(Date.now()), duration);
 };
